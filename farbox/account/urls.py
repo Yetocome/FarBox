aa524658e7b93cdc8e4f3cc3b9b5bccb629e8614
mathon <luoxinchen96@gmail.com>
2016-04-28 19:35:47 +0800

    login && register

--- farbox/account/urls.py
+++ farbox/account/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url, include
 from account import views
 
 urlpatterns = [
-    url(r'^$', views.register, name='register'),
-    url(r'^register/$', views.register, name='register'),
-    url('^', include('django.contrib.auth.urls'))
+    url(r'^$', views.home, name='home'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^login/', views.login, name='login'),
 ]
--- farbox/account/views.py
+++ farbox/account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django import forms
 from django.shortcuts import render_to_response
-from account.models import User
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
@@ -10,6 +11,12 @@ class UserForm(forms.Form):
     password = forms.CharField(label='密码: ', widget=forms.PasswordInput())
     email = forms.EmailField(label='电子邮件: ')
 
+class LoginForm(forms.Form):
+    username = forms.CharField(label='用户名: ', max_length=100)
+    password = forms.CharField(label='密码: ', widget=forms.PasswordInput())
+
+def home(request):
+    return render(request, 'index.html')
 def register(request):
     if request.method == 'POST':
         uf = UserForm(request.POST)
@@ -21,7 +28,7 @@ def register(request):
             #将表单写入数据库
             user = User()
             user.username = username
-            user.password = password
+            user.set_password(password)
             user.email = email
             user.save()
             #返回注册成功页面
@@ -29,3 +36,21 @@ def register(request):
     else:
         uf = UserForm()
     return render(request, 'register.html',{'uf':uf})
+
+def login(request):
+    if request.method == 'POST':
+        lf = LoginForm(request.POST)
+        if(lf.is_valid()):
+            username = lf.cleaned_data['username']
+            password = lf.cleaned_data['username']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                msg = user.username + ' login Success'
+            else:
+                msg = 'Username or password wrong'
+        else:
+            msg = 'Internal Error'
+        return render(request, 'msg.html', {'msg':msg})
+    else:
+        lf = LoginForm()
+    return render(request, 'login.html', {'lf':lf})
