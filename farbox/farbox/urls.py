f06358c4a3840e108b43e793fc93b7e768804f3f
mathon <luoxinchen96@gmail.com>
2016-05-17 10:11:45 +0800

    modified urls

--- farbox/farbox/urls.py
+++ farbox/farbox/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^farbox/', include('FarboxWeb.urls')),
+    url(r'^', include('FarboxWeb.urls')),
 ]
