8c289429b2fa6a32accf77ac0cd6f6cba5069792
jibancanyang <1599664856@qq.com>
2016-05-26 15:45:29 +0800

    test.cpp's test

--- test.cpp
+++ test.cpp
@@ -1,1 +1,22 @@
-#Just a test
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+#define pr(x) cout << #x << ": " << x << "  " 
+#define pl(x) cout << #x << ": " << x << endl;
+const int maxn = 1e5 + 13;
+
+
+int main()
+{
+#ifdef xiaoai
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+#endif
+    string str(100, 'x');
+    auto strp = str.rbegin();
+    cout << *strp << endl;
+    return 0;
+}
