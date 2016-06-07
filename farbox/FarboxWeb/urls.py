96f6aa30091383ab49ce02922d6e1abd4edda6b5
mathon <luoxinchen96@gmail.com>
2016-06-07 09:07:36 +0800

    mathon

--- farbox/FarboxWeb/urls.py
+++ farbox/FarboxWeb/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index', views.index, name='index'),
     url(r'^register/', views.register, name='register'),
     url(r'^login/', views.login, name='login'),
     url(r'^upload', views.upload, name='upload'),
